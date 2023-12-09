c6482f8d674e67c8c691edd73da0095bf0a3d3ac
Alan Peine <alan.peine.ext@bayer.com>
2023-12-08 23:58:50 -0600

    p2 complete, okay actually that worked and was simple. Nice.

--- day_09/predict.go
+++ day_09/predict.go
@@ -10,32 +10,37 @@ import (
 func main(){
 	//part 1
 	herstories := parseLines(readInput())
+	past, future := predictHerFuture(herstories)
 	//part 2
-	fmt.Println(sum(predictHerFuture(herstories)))
-	fmt.Println()
+	fmt.Println(sum(future))
+	fmt.Println(sum(past))
 }
-func predictHerFuture(herstories [][]int) []int {
+func predictHerFuture(herstories [][]int) ([]int, []int){
 	herFutures := make([]int, len(herstories))
+	herPast := make([]int, len(herstories))
 
 	for i, v := range herstories {
-		fmt.Println(v)
-		herFutures[i] = predict(v)
+		before, next := predict(v)
+		herPast[i] = before
+		herFutures[i] = next
 	}
 
-	return herFutures
+	return herPast, herFutures
 }
 //recursion day?
-func predict(herstory []int) int {
+func predict(herstory []int) (int, int) {
 	//if the differences are all 0, add a zero, return?
 	if onlyContains(herstory, 0) {
-		return 0
+		return 0, 0
 	}
 	//find the differences, predict by adding return value to last element
 	differences := make([]int, len(herstory) - 1)
 	for i, _ := range differences {
 		differences[i] = herstory[i+1] - herstory[i]
 	}
-	return herstory[len(herstory) - 1] + predict(differences)
+	//part 2 just added a double return and always used 0 index - first return value, worked first try
+	before, next := predict(differences)
+	return herstory[0]-before, herstory[len(herstory) - 1] + next
 }
 
 func onlyContains(array []int, value int) bool {
