fe205ecb3849857943f4131a4936908abff149bc
Alan Peine <alan.peine.ext@bayer.com>
2023-12-10 15:39:09 -0600

    Day 10 p1 complete. P2 looks, difficult. Definately a math focused problem, possibly topology related? When is a hole a hole and all that

---
+++ day_10/tubes.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main(){
+	//part 1
+	start, length := mapPipe(parseLines(readInput()))
+	fmt.Println(start, length)
+	fmt.Println(length/2)
+
+	//part 2
+}
+
+func mapPipe(start Pipe, field [][]rune) (looped Pipe, len int){
+	len = 1//because we add the first connection manually
+	x, y := findFirstConnection(start, field)
+	next := Pipe{x, y, field[y][x], &start, nil}
+	start.b = &next
+	for next.Symbol != 'S' {
+		next = nextPipe(next, field)
+		len+=1
+	}
+	//overwrite end just to do a full loop
+	next.b = &start
+
+	return start, len
+}
+
+func nextPipe(current Pipe, field [][]rune) Pipe {
+	nextX, nextY := 0, 0
+	switch current.Symbol {
+	case '|':
+		nextX = current.X
+		//headed North?
+		if current.a.Y > current.Y {
+			nextY = current.Y-1
+		}else {
+			nextY = current.Y+1
+		}
+	case '-':
+		nextY = current.Y
+		//headed west?
+		if current.a.X > current.X {
+			nextX = current.X - 1
+		}else {
+			nextX = current.X + 1
+		}
+	case 'L':
+		//headed west?
+		if current.a.X > current.X {
+			nextX = current.X
+			nextY = current.Y -1
+		}else {
+			nextX = current.X + 1
+			nextY = current.Y
+		}
+	case 'J':
+		//headed south?
+		if current.a.Y < current.Y {
+			nextX = current.X - 1
+			nextY = current.Y
+		}else {
+			nextX = current.X
+			nextY = current.Y -1
+		}
+	case '7':
+		if current.a.X < current.X {
+			nextX = current.X
+			nextY = current.Y + 1
+		}else {
+			nextY = current.Y
+			nextX = current.X - 1
+		}
+	case 'F':
+		if current.a.Y > current.Y {
+			nextY = current.Y
+			nextX = current.X + 1
+		}else {
+			nextY = current.Y + 1
+			nextX = current.X
+		}
+	case '.':
+		panic("Shouldn't have gotten here")
+	}
+
+	next := Pipe{nextX, nextY, field[nextY][nextX], &current, nil}
+	current.b = &next
+	return next
+
+}
+func findFirstConnection(start Pipe, field[][]rune) (x,y int) {
+	switch  {
+	case strings.ContainsAny(string(field[start.Y-1][start.X]),  "|7F")://go north first
+		return start.X, start.Y-1
+	case strings.ContainsAny(string(field[start.Y+1][start.X]),  "|LJ")://go south first
+		return start.X, start.Y + 1
+	case strings.ContainsAny(string(field[start.Y][start.X-1]),  "-LF")://go west first
+		return start.X -1, start.Y
+	case strings.ContainsAny(string(field[start.Y][start.X+1]),  "-J7")://go east first
+		return start.X + 1, start.Y
+	default:
+		panic("no starts")
+	}
+}
+func parseLines(lines string) ( start Pipe, field [][]rune) {
+	field = make([][]rune, 0)
+	for i, line := range strings.Split(lines, "\n") {
+		row := make([]rune, 0)
+		for j, char := range line {
+			row = append(row, char)
+			if char == 'S' {
+				start = Pipe{j, i, char, nil, nil}
+			}
+		}
+		field = append(field, row)
+	}
+	return start, field
+}
+type (
+	Pipe struct {
+		X, Y int
+		Symbol rune
+		a *Pipe
+		b *Pipe
+
+}
+)
+func readInput() string {
+	var filename string
+	if len(os.Args) < 2 {
+		fmt.Println("Assuming local file input.txt")
+		filename = "./input.txt"
+	}else{
+		filename = os.Args[1]
+	}
+
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Can't read file:", filename)
+		panic(err)
+	}
+
+	//return and account for windows
+	return strings.ReplaceAll(string(data), "\r\n", "\n")
+}
