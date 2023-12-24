b73825e0d92810cc999243213dd454c5453e8e98
Alan Peine <alan.peine.ext@bayer.com>
2023-12-23 19:36:00 -0600

    Day 23 Part 2 complete. Dumb day, made dumber by myself. I knew right after part 1 that my solution was way too long and got stuck in that idea for way too long trying to make it more performant and multithreadig it. Fun to learn about channels and more about go routines. Messed with it further, turning it into nodes at branch points. Finally just scrapped and started just from thinking on nodes and recording lengths to each node. Reddit helped.

--- day_23/hike.go
+++ day_23/hike.go
@@ -1,164 +1,126 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
 func main(){
 	//part 1
 	trail, start, end := parseLines(readInput())
-	longest := gohike(trail, start, end, true)
-	fmt.Println(longest)
-	//print(path, trail)
+	fmt.Println(findLength(trail, start, end, true))
 
 
 	//part 2
-	longest = gohike(trail, start, end, false)
-	fmt.Println(longest)
+	fmt.Println(findLength(trail, start, end, false))
+
 }
 
-func gohike(trail map[Point]Trail, start, end Point, steepSlopes bool)(int) {
-	longest := make(chan int, 1)
-	longest <- 0
-	//longest := Path{map[Point]bool{}, start}
-	first := Path{map[Point]bool{start:true}, start}
-	//queue := []Path{first}
-	chanQueue := Queue{make(chan Path, 10000)}
-	chanQueue.Insert(first)
-	for  {
-		go makeStep(trail, steepSlopes, chanQueue, end, longest)
+func hike(nodes map[Point][]Cost, current, end Point, visited map[Point]bool, cost, max int) int {
+	if current == end {
+		if cost > max {
+			max = cost
+		}
+		return max
 	}
-	test:= <- longest
-	return test
+
+	visited[current] = true
+
+	for _, to := range nodes[current] {
+		if !visited[to.Point] {
+			max = hike(nodes, to.Point, end, visited, cost + to.Cost, max)
+		}
+	}
+	visited[current] = false
+	return max
 }
 
-func hike(trail map[Point]Trail, start, end Point, steepSlopes bool)(int, Path) {
-	longest := Path{map[Point]bool{}, start}
-	first := Path{map[Point]bool{start:true}, start}
-	queue := []Path{first}
+func findLength(trail map[Point]Trail, start, end Point, steepSlopes bool) int {
+	cost := 0
+	nodes := findNodes(trail, start, steepSlopes)
+
+	//skip end
+	if !steepSlopes && len(nodes[end]) > 0 {
+		cost = nodes[end][0].Cost
+		end = nodes[end][0].Point
+	}
+
+	visited := map[Point]bool{}
+	return hike(nodes, start, end, visited, cost, 0)
+}
+
+func findNodes(trail map[Point]Trail, start Point, steepSlopes bool) map[Point][]Cost {
+	costs := map[Point][]Cost{}
+	queue := []Point{start}
+
 	for len(queue) > 0 {
 		current := queue[0]
 		queue = queue[1:]
 
-		//we reached the end the slowest so far
-		if current.current == end && len(longest.visited) < len(current.visited){
-			fmt.Println("Found end: ", len(current.visited) - 1, " Queue Length: ", len(queue))//dont count start
-			longest = current
-		}
-
-		nextPossibilities := possibleDirections(current, trail, steepSlopes)
-		//skips copying the whole path if we aren't at a branch
-		if len(nextPossibilities) > 1 {
-			for _, nextPoint := range nextPossibilities {
-				//take the current path and add it, add to queue
-				visited := current.copyVisited()
-				visited[nextPoint] = true
-				queue = append(queue, Path{visited, nextPoint})
-			}
-		} else if len(nextPossibilities) == 1 {
-			current.current = nextPossibilities[0]
-			current.visited[nextPossibilities[0]] = true
-			queue = append(queue, current)
-		}
-
-		//fmt.Println(len(queue))
-
-	}
-	return len(longest.visited) - 1, longest
-}
-
-func makeStep(trail map[Point]Trail, steepSlopes bool, queue Queue, end Point, longest chan int)  {
-	current, err := queue.Remove()
-	if err != nil {
-		return
-	}
-	//we reached the end the slowest so far
-	if current.current == end {
-		oldLongest := <- longest
-		fmt.Println("Found end: ", len(current.visited) - 1, "was: ", oldLongest ," Queue Length: ", len(queue.q))//dont count start
-		longest <- len(current.visited) - 1
-	}
-
-	nextPossibilities := possibleDirections(current, trail, steepSlopes)
-	//skips copying the whole path if we aren't at a branch
-	if len(nextPossibilities) > 1 {
-		for _, nextPoint := range nextPossibilities {
-			//take the current path and add it, add to queue
-			visited := current.copyVisited()
-			visited[nextPoint] = true
-			queue.Insert(Path{visited, nextPoint})
-		}
-	} else if len(nextPossibilities) == 1 {
-		current.current = nextPossibilities[0]
-		current.visited[nextPossibilities[0]] = true
-		queue.Insert(current)
-	}
-	return
-}
-
-type Queue struct {
-	q        chan Path
-}
-
-func (q *Queue) Insert(item Path)  {
-		q.q <- item
-}
-
-func (q *Queue) Remove() (Path, error) {
-	if len(q.q) > 0 {
-		item := <-q.q
-		return item, nil
-	}
-	return Path{}, errors.New("Queue is empty")
-}
-//if this causes bottleneck we could do it only on branch points?
-func (this Path) copyVisited() (copy map[Point]bool){
-	copy = make(map[Point]bool, len(this.visited))
-	for key, value := range this.visited {
-		copy[key] = value
-	}
-	return
-}
-func possibleDirections(path Path, trail map[Point]Trail, steepSlopes bool) []Point{
-	this := path.current
-	possibilities := make([]Point, 0)
-	//had this as a real switch but changed in part 2 to account for ignoring the slopes
-	switch {
-	case steepSlopes && trail[this].char == '^':
-		possibilities = append(possibilities, this.add(North))
-	case steepSlopes && trail[this].char == '>':
-		possibilities = append(possibilities, this.add(East))
-	case steepSlopes && trail[this].char == 'v':
-		possibilities = append(possibilities, this.add(South))
-	case steepSlopes && trail[this].char == '<':
-		possibilities = append(possibilities, this.add(West))
-	default:
-		//check possibles
 		for _, dir := range directions {
-			next := this.add(dir)
+			next := current.add(dir)
 			if trail[next].char != '#' {
-					possibilities = append(possibilities, next)
+				cost, valid := nodesPath(trail, current, next, 1, steepSlopes)
+				if valid && !slices.Contains(costs[current], cost ) {
+					costs[current] = append(costs[current], cost)
+					queue = append(queue, cost.Point)
+				}
 			}
 		}
 	}
-	//cant backtrack, doing here because i allowed backtracks specifically on slope force movement and wanted once check
-	possibilitiesNoBacktracking := make([]Point, 0)
-	for _, possibility := range possibilities {
-		if _, exists := path.visited[possibility]; !exists{
-			possibilitiesNoBacktracking = append(possibilitiesNoBacktracking, possibility)
+	return costs
+}
+
+func nodesPath(trail map[Point]Trail, previous, current Point, cost int, steepSlopes bool) (Cost, bool) {
+	options := 0
+	for _, dir := range directions {
+		next := current.add(dir)
+		if trail[next].char != '#' {
+			options += 1
 		}
 	}
-	return possibilitiesNoBacktracking
+	//node point
+	if options > 2 {
+		return Cost{current, cost}, true
+	}
 
+	if steepSlopes {
+		char := trail[current].char
+		if char != '.' {
+			if current.X > previous.X && char != '>' {
+				return Cost{}, false
+			}
+			if current.X < previous.X && char != '<' {
+				return Cost{}, false
+			}
+			if current.Y > previous.Y && char != 'v' {
+				return Cost{}, false
+			}
+			if current.Y < previous.Y && char != '^' {
+				return Cost{}, false
+			}
+		}
+	}
+
+	//follow path
+	for _, dir := range directions {
+		next := current.add(dir)
+		if trail[next].char != '#' && next != previous {
+			return nodesPath(trail, current, next, cost + 1, steepSlopes)
+		}
+	}
+
+	return Cost{current, cost}, true
 }
-//func print(path Path, trail map[Point]Trail) {
-//	for y:= 0; y < 142; y ++ {//24 for test input
-//		for x:=0; x < 142; x++ {//24 for test input
+
+
+//func print(visited map[Point]bool, trail map[Point]Trail) {
+//	for y:= 0; y < 24; y ++ {//24 for test input,142
+//		for x:=0; x < 24; x++ {//24 for test input,142
 //			point := Point{x,y}
-//			if _, exists := path.visited[point]; exists {
+//			if _, exists := visited[point]; exists {
 //				fmt.Print("O")
 //			}else {
 //				fmt.Print(string(trail[point].char))
@@ -167,6 +129,7 @@ func possibleDirections(path Path, trail map[Point]Trail, steepSlopes bool) []Po
 //		fmt.Println()
 //	}
 //}
+
 
 func (this Point) add(that Point) Point {
 	return Point{this.X + that.X, this.Y + that.Y}
@@ -220,13 +183,13 @@ type (
 	Point struct {
 		X,Y int
 	}
+	Cost struct {
+		Point
+		Cost int
+	}
 	Trail struct {
 		Point
 		char rune
-	}
-	Path struct {
-		visited map[Point]bool
-		current Point
 	}
 )
 
